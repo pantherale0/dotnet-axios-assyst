e8c04f28754ac689d924c5fc0e3bd7ed2ed57b38
Jordan Harvey <jordan.harvey@jordanharvey.xyz>
2021-12-08 15:18:55 +0000

    Update customFields

---
+++ axios-assyst/Models/_customFields.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axios_Assyst.Models
+{
+    public class _customFields
+    {
+        public string customFieldShortCode { get; set; }
+    }
+
+    public class _customFieldString : _customFields
+    {
+        public string stringValue { get; set; }
+    }
+
+    public class _customFieldLookup : _customFields
+    {
+        public string customLookupValueShortCode { get; set; }
+    }
+}
